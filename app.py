aaf580060cd417847410e3ff30949583d67d2973
Luis Orna <prof.luis.orna@gmail.com>
2025-09-02 08:15:33 -0300

    Avance martes

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask
+#Dependencias
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 #Creo la app
 app = Flask(__name__)
 app.config.from_object(Config) # Pido que la app se configure desde el objeto Config
 
+#Creo el objeto para hablar con la base de datos
+db = SQLAlchemy(app)
 
 #Ruta de Prueba
 @app.route('/')
 def home():
+    print(app.config['SECRET_KEY'])
     return "Hola Passport"
+
+#Ruta para loging de Prueba
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        return f'Login exitoso {email}'
+
+
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,2 +1,5 @@
+#Creo la clase principal que se recibe en app.py
 class Config:
     SECRET_KEY = 'mi-clave-secreta-temporal'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/passport.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  #Para que no me consuma memoria vigilando 
